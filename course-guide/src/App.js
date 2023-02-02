aca1336cb17290610736bcc896742283cb2c1b58
Gwali <gwalisam37@gmail.com>
2023-02-02 11:31:54 +0000

    base commit

---
+++ course-guide/src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <div className="App">
+      <h2>let get started</h2>
+      <p>This is visible</p>
+    </div>
+  );
+}
+
+// function Custom() {
+//   return (
+//     <>
+//       <h2>This is a custom component</h2>
+//       <p>I created it myself</p>
+//     </>
+//   );
+// }
+
+export default App;
